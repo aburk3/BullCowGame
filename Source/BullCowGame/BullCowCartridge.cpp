9ace51517761fb75611ee7d23b22bdde41602d66
Austin Burke <aburk3@gmail.com>
2021-09-14 05:55:37 -0500

    Added setupgame function

--- Source/BullCowGame/BullCowCartridge.cpp
+++ Source/BullCowGame/BullCowCartridge.cpp
@@ -10,9 +10,7 @@ void UBullCowCartridge::BeginPlay() // When the game starts
     PrintLine(TEXT("Guess the 4 letter word!")); // Magic Number Remove!
     PrintLine(TEXT("Press enter to continue..."));
 
-    // Setting Up Game
-    HiddenWord = TEXT("cake"); // Set the HiddenWord
-    // Set Lives
+    SetupGame();
 
     // Pomprt Player for Guess
 }
@@ -46,4 +44,10 @@ void UBullCowCartridge::OnInput(const FString& Input) // When the player hits en
     // Prompt to Play Again, Press Enter To Play Again?
     // Check User Input
     // PlayAgain Or Quit
+}
+
+void UBullCowCartridge::SetupGame()
+{
+    HiddenWord = TEXT("cake");
+    Lives = 4;
 }
